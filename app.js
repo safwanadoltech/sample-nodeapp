23192d3b67e0f6ceb52e036f022e794151c010bb
safwanadoltech <safwan@adoltech.com>
2023-10-30 16:10:19 +0530

    test-nodeapp

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change the port to any port you prefer
+
+app.get('/', (req, res) => {
+  res.send('Hello, 1234');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
