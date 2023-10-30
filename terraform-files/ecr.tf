fa81e127582275bb25f19e36c0085eac32f3b023
safwanadoltech <safwan@adoltech.com>
2023-10-30 17:19:27 +0530

    first-deployment

--- terraform-files/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "my-ecr-repo" {
-name = "my-ecr-repo"                         #replace with the desired name
-image_tag_mutability = "MUTABLE"
-}
--- terraform-files/s3.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
-}
- 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-state-bucket-1234"  # Replace with a unique bucket name
-}
-
