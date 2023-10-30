db80b38d7bb605847cca160eb644d73539ee86ca
safwanadoltech <safwan@adoltech.com>
2023-10-30 17:00:04 +0530

    first-deployment

--- terraform-files/s3-backend.tf
+++ terraform-files/s3-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket-1234" #replace with your s3 bucket name
-    key    = "terraform.tfstate"
+    key    = "terraform.tfstate-file"
     region = "us-east-1"
   }
 }
