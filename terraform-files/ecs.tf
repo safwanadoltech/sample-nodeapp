18d301fd776e1bb8a6720abbd8e80e516fad8d1f
safwanadoltech <safwan@adoltech.com>
2023-10-31 12:48:57 +0530

    first-deployment

--- terraform-files/ecs.tf
+++ terraform-files/ecs.tf
@@ -113,8 +113,8 @@ vpc_id=module.vpc.vpc_id
 
 resource "aws_lb_listener" "my_listener" {
 load_balancer_arn=aws_lb.my_lb.arn
-port=443
-protocol="HTTPS"
+port=80
+protocol="HTTP"
 default_action {
 type="forward"
 target_group_arn=aws_lb_target_group.my_target_group.arn
