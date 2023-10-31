4604998160095dfaee902a0f7edc1477c3ab5dd1
safwanadoltech <safwan@adoltech.com>
2023-10-31 11:50:29 +0530

    first-deployment

--- terraform-files/securitygroup.tf
+++ terraform-files/securitygroup.tf
@@ -9,6 +9,18 @@ resource "aws_security_group" "ecs_tasks" {
     to_port = 3000
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    protocol = "tcp"
+    from_port = 80        #your app's port number
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol = "tcp"
+    from_port = 443        #your app's port number
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     protocol = "-1"
