34efe378bd8a2480b6797c4c03ef482f001149f2
safwanadoltech <safwan@adoltech.com>
2023-10-31 16:08:46 +0530

    Edited app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port to any port you prefer
+const port = 3000; // You can change the port as needed
 
+// Define a route that responds with "Hello, World!"
 app.get('/', (req, res) => {
-  res.send('Hello, world');
+  res.send('Hello from the other side');
 });
 
+// Start the Express server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
